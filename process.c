39e546cd9b4d3f66718b9c0388359110f07cbe4a
Nikolay Valaev <niklada9@mail.ru>
2021-11-26 00:56:52 +0300

    proc fin

--- process.c
+++ process.c
@@ -8,7 +8,7 @@ const int GLOBAL = 0xdeadbeef;
 int main (int argc, char *argv[]) {
 
 int i;
-int (*g)(void) = &GLOBAL;
+//int (*g)(void) = &GLOBAL;
 
 printf("global -> %x\n", (unsigned int)&global); 
 printf("Global -> %x\n", (unsigned int)&Global);
@@ -18,7 +18,7 @@ printf("argc -> %x\n", (unsigned int)&argc);
 printf("argv -> %x\n", (unsigned int)&argv);
 printf("main -> %x\n", (unsigned int)&main);
 
-g();
+//g();
 
 for(;;);
 

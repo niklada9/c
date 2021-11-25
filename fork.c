01c50413574fa9693d75824ec417eb0f82b1c9dc
Nikolay Valaev <niklada9@mail.ru>
2021-11-25 23:00:06 +0300

    fork if

--- fork.c
+++ fork.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void parent(void){
+    for(;;) printf("parent %d\n", pid);
+}
+
+void child(void){
+    for(;;) printf("child %d\n", pid);
+}
+
 int main(){
 
 int pid;
 pid = fork();
-for(;;) printf("%d\n", pid);
+    if (pid ==0)
+    {
+       child
+    }
+    else{parent}
+
+//for(;;) printf("%d\n", pid);
+return 0;
 }
 //remark
